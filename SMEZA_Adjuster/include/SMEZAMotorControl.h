a7fb27893f18c6152482479885021fd268a18eeb
Joel R-S VU <joel.reuningscherer@valpo.edu>
2023-02-28 13:27:16 -0600

    Create Motor Control Files

---
+++ SMEZA_Adjuster/include/SMEZAMotorControl.h
@@ -0,0 +1,9 @@
+/* SMEZA Motor Control 
+ * 
+ */
+
+#ifndef SMEZAMOTORCONTROL_H
+#define SMEZAMOTORCONTROL_H
+
+#endif
+// End of File
---
+++ SMEZA_Adjuster/src/SMEZAMotorControl.cpp
@@ -0,0 +1,8 @@
+/* SMEZA Motor Control 
+ * 
+ */
+
+// Code will go here
+
+
+// End of File
